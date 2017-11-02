78b8fda0e714c437fbce4c332ef462588b726795
Glitch (saber-stitch) <none>
2017-11-02 22:09:58 +0000

    💫🏵 Updated with Glitch

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
-require('dotenv').config();
 const IRC = require('irc-framework');
 const skateboard = require('skateboard');
+const fetch = require('fetch-retry');
 
 const bot = new IRC.Client();
 let count = 0;
 let socket = null;
 
-function botConnect(callback) {
+const getConfig = fetch(`https://configapi.glitch.me/config/${process.env.PROJECT_ID}`, {
+    retries: 3,
+    retryDelay: 1000
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    botConnect(data, () => {
+      console.log('joined channel!');
+      
+      skateboard({port: 3000}, (stream) => {
+      socket = stream;
+    });
+
+    bot.matchMessage(/^\d\d?$/, (event) => {
+      const n = parseInt(event.message);
+      count = (n - count === 1) ? n : 0;
+      socket.write(count.toString()); 
+    });
+  });
+});
+
+function botConnect(config, callback) {
   bot.connect({
     host: 'irc.chat.twitch.tv',
     port: 6667,
-    nick: process.env.TWITCH_NICK,
-    password: process.env.TWITCH_TOKEN
+    nick: config.TWITCH_NICK,
+    password: config.TWITCH_TOKEN
   });
 
   bot.on('registered', () => {
     console.log('reg happened!');
+    bot.requestCap('twitch.tv/membership');
 
-    const channel = bot.channel('#twitchcongiveplz');
+    const channel = bot.channel(config.TWITCH_CHAN);
     channel.join();
     return callback();
   });
 };
-botConnect(() => {
-  console.log('joined channel!');
-
-  skateboard({port: 3000}, (stream) => {
-    socket = stream;
-  });
-
-  bot.matchMessage(/^\d\d?$/, (event) => {
-    const n = parseInt(event.message);
-    count = (n - count === 1) ? n : 0;
-    socket.write(count.toString()); 
-  });
-});
-
-
